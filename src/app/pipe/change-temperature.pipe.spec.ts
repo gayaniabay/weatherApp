b8370fcc49d35039032a9903dfcee04fa7bef166
Gayani Abayagunawardhana <gayani.aba@gmail.com>
2019-07-26 07:57:47 +0530

    Deleted pipe

--- src/app/pipe/change-temperature.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeTemperaturePipe } from './change-temperature.pipe';
-
-describe('ChangeTemperaturePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ChangeTemperaturePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
